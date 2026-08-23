fb0fcffba7db9dcf6c9d12fdb28b6683dfdba2a6
Aryan Karfa <xtrmakarov@gmail.com>
2026-08-23 21:01:24 +0530

    Update appConfig with new name and codename

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
-  name: 'RAKTKOSH',
-  temporaryCodename: 'RAKTKOSH',
+  name: 'ROKTO R DAKAT',
+  temporaryCodename: 'ROKTO R DAKAT',
   productCategory: 'Smart Blood-Bank Inventory & Demand Intelligence Platform',
   version: '0.1.0-alpha',
   networkName: 'Kolkata Regional Blood Network',
