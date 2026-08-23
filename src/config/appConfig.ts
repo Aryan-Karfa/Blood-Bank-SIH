bb034f8e65e9469273a6f5b777378b8b0f5fb333
Aryan Karfa <xtrmakarov@gmail.com>
2026-08-23 21:14:26 +0530

    Update APP_CONFIG name and codename to 'TRANSFUSE'

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
-  name: 'A+B-O',
-  temporaryCodename: 'A+B-O',
+  name: 'TRANSFUSE',
+  temporaryCodename: 'TRANSFUSE',
   productCategory: 'Smart Blood-Bank Inventory & Demand Intelligence Platform',
   version: '0.1.0-alpha',
   networkName: 'Kolkata Regional Blood Network',
